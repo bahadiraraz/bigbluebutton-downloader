c560b3349f73e3bb01d3c2956ba4ac6c05727744
BAHADIRking <b123qweasdzxc@gmail.com>
2021-11-12 18:17:41 +0300

    files added

---
+++ main.ps1
@@ -0,0 +1,86 @@
+# Functions
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+function DownloadFile
+{
+    param($downloadUrl, $output, $description)
+    Write-Host "$description indiriliyor..."
+    Invoke-WebRequest -Uri $downloadUrl -OutFile $output
+}
+
+
+
+while ($true) {
+    Write-Host "yapimci bahadir araz :)"
+    Write-Output https://github.com/bahadiraraz
+    $bbbUrl = Read-Host -Prompt 'video linkini giriniz'
+    $resultFileName = Read-Host -Prompt 'video kayit isimi'
+    $resultFileName += ".mp4"
+    $parts = $bbbUrl -split "meetingId="
+    $meetingId = $parts[1]
+    $bbbHost = $parts[0] -split "/playback"
+    $bbbHost = $bbbHost[0]
+
+    Write-Host "MeetingID: $meetingId"
+    Write-Host "BBB-Host: $bbbHost"
+
+    # sesi indirme
+
+    $webcamsUrl = "$bbbHost/presentation/$meetingID/video/webcams.mp4"
+    $output = "webcams.mp4"
+
+    DownloadFile $webcamsUrl $output "ses"
+
+    # goruntu indirme
+
+    $deskshareUrl = "$bbbHost/presentation/$meetingID/deskshare/deskshare.mp4"
+    $output = "deskshare.mp4"
+
+    DownloadFile $deskshareUrl $output "video"
+
+    # FFMPEG yok ise yukle
+
+    $ffmpegPath = ".\ffmpeg.exe"
+    $ffmpegVersion = "4.3.1-2020-11-08"
+
+    if (Test-Path($ffmpegPath)) {
+        Write-Host "$ffmpegPath, hazida indirili"
+        Write-Host "islem baslatiliyor..."
+    } else {
+        $ffmpegUrl = "https://github.com/GyanD/codexffmpeg/releases/download/$ffmpegVersion/ffmpeg-$ffmpegVersion-full_build.zip"
+        $output = "ffmpeg.zip"
+
+        DownloadFile $ffmpegUrl $output "ffmpeg.zip"
+
+        # Unzipping and moving
+
+        Write-Host "disa aktariliyor ffmpeg.zip..."
+        Unzip ".\$output" "."
+
+        Write-Host "yer degistiriliyor ffmpeg.exe..."
+        Move-Item -Path ".\ffmpeg-$ffmpegVersion-full_build\bin\ffmpeg.exe" -Destination "."
+
+        Write-Host "siliniyor ffmpeg.zip..."
+        Remove-Item -Recurse -Force ".\ffmpeg-$ffmpegVersion-full_build"
+        Remove-Item -Force ".\ffmpeg.zip"
+    }
+
+    # Convert to one videofile
+
+    Write-Host "ses ve goruntu birlestiriliyor..."
+    Invoke-Expression ".\ffmpeg.exe -loglevel quiet -i webcams.mp4 -i deskshare.mp4 -c copy $resultFileName"
+    #delete webcams.mp4 deskshare.mp4
+    Remove-Item -Force ".\webcams.mp4"
+    Remove-Item -Force ".\deskshare.mp4"
+    Write-Host "islem tamamlandi."
+    Write-Host ""
+    $host.UI.RawUI.ClearHost
+    $host.UI.RawUI.Clear
+}
---
+++ run.bat
@@ -0,0 +1,6 @@
+#run main.ps1 file
+
+@echo off
+#start main.ps1 file with new powershell
+start pwsh.exe -file main.ps1
+timeout /t 5
